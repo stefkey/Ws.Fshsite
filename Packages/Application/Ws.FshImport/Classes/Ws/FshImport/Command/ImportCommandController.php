0d42e8d3d6086cb4b78a15c9a53eb9ae6cc4f365
Dmitri Pisarev <dimaip@gmail.com>
2016-09-20 23:46:00 +0300

    TASK: import accounts
    Import the database dumps and then run `./flow import:importaccounts`

---
+++ Packages/Application/Ws.FshImport/Classes/Ws/FshImport/Command/ImportCommandController.php
@@ -0,0 +1,52 @@
+<?php
+namespace Ws\FshImport\Command;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Cli\CommandController;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class ImportCommandController extends CommandController {
+
+	/**
+	 * @Flow\Inject
+	 * @var Sandstorm\UserManagement\Command\SandstormUserCommandController
+	 */
+	protected $userManagementController;
+
+	/**
+	 * @Flow\Inject(lazy = FALSE)
+	 * @var \Doctrine\Common\Persistence\ObjectManager
+	 */
+	protected $entityManager;
+
+	/**
+	 * Import accounts
+	 *
+	 * @return string
+	 */
+	public function importAccountsCommand() {
+		$accounts = $this->getAccounts();
+		foreach ($accounts as $account) {
+			$username =  html_entity_decode(urldecode($account['username']));
+			$name = html_entity_decode(urldecode($account['name']));
+			$nameArray = explode(" ", $name, 2);
+			$firstName = $nameArray[0];
+			$lastName = isset($nameArray[1]) ? $nameArray[1] : '-';
+			try {
+				$this->userManagementController->createCommand($username, 'dontknowhowtosetittonull', $firstName, $lastName);
+			} catch (\Exception $e) {
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+		}
+		return "Done!";
+	}
+
+	protected function getAccounts() {
+		$sql = "SELECT f.username, n.confirmeddate, n.name FROM con_frontendusers AS f JOIN con_news_rcp AS n ON f.username = n.email WHERE f.active = 1";
+		$statement = $this->entityManager->getConnection()->prepare($sql);
+		$statement->execute();
+		return $statement->fetchAll(\PDO::FETCH_ASSOC);
+	}
+}
