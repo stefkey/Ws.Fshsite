85cc81ccec2db6e4b45004050db43af69773c17b
Florian Heinze <florian.heinze@sandstorm-media.de>
2019-03-25 15:53:37 +0100

    TASK: docker setup to get versions right

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+composer install
+
+echo "---------------------> Give Database a Headstart"
+sleep 3
+
+#./flow doctrine:migrate
+#./flow site:import --package-key Konglomerat.Website
+#./flow user:create --roles Administrator admin password LocalDev Admin
+#./flow resource:publish
+
+./flow server:run --host=0.0.0.0
