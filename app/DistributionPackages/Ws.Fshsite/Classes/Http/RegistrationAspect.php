7cbbc8abca42289b4c6bedd1ed4a325a2ceb0a15
Robert Baruck <robert.baruck@sandstorm-media.de>
2019-12-23 19:09:23 +0100

    TASK: New registration process

--- app/DistributionPackages/Ws.Fshsite/Classes/Http/RegistrationAspect.php
+++ app/DistributionPackages/Ws.Fshsite/Classes/Http/RegistrationAspect.php
@@ -3,11 +3,13 @@
 namespace Ws\Fshsite\Http;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\DriverManager;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\AOP\JoinPointInterface;
 use Neos\Flow\Persistence\PersistenceManagerInterface;
 use Sandstorm\TemplateMailer\Domain\Service\EmailService;
+use Sandstorm\UserManagement\Controller\RegistrationController;
+use Sandstorm\UserManagement\Domain\Model\RegistrationFlow;
 use Sandstorm\UserManagement\Domain\Repository\RegistrationFlowRepository;
 use Sandstorm\UserManagement\Domain\Service\UserCreationServiceInterface;
 
@@ -17,12 +19,6 @@ use Sandstorm\UserManagement\Domain\Service\UserCreationServiceInterface;
  */
 class RegistrationAspect
 {
-
-    /**
-     * @Flow\Inject
-     * @var \Neos\Flow\Log\SystemLoggerInterface
-     */
-    protected $systemLogger;
 
     /**
      * @Flow\Inject
@@ -49,6 +45,12 @@ class RegistrationAspect
     protected $databaseSettings;
 
     /**
+     * @Flow\InjectConfiguration(package="Ws.Fshsite", path="fshRegistrationMailRecipient")
+     * @var string
+     */
+    protected $fshRegistrationMailRecipient;
+
+    /**
      * @var Connection
      */
     protected $connection;
@@ -59,61 +61,193 @@ class RegistrationAspect
      */
     protected $persistenceManager;
 
-    /**
-     * Log a message if a post is deleted
-     *
-     * @param \Neos\Flow\AOP\JoinPointInterface $joinPoint
-     * @Flow\Around("method(Sandstorm\UserManagement\Controller\RegistrationController->activateAccountAction())")
-     * @return void
-     */
-    public function logRegistration(\Neos\Flow\AOP\JoinPointInterface $joinPoint)
-    {
-        /* @var $registrationFlow \Sandstorm\UserManagement\Domain\Model\RegistrationFlow */
-        $registrationFlow = $this->registrationFlowRepository->findOneByActivationToken($joinPoint->getMethodArgument('token'));
-        if (!$registrationFlow) {
-            //TODO
-            return;
-        }
+    // Membership states
+    const UNKNOWN = 'UNKNOWN';
+    const IS_MEMBER = 'IS_MEMBER';
+    const NOT_MEMBER = 'NOT_MEMBER';
 
-        if (!$registrationFlow->hasValidActivationToken()) {
-            //TODO
-            return;
+    /**
+     * New registration workflow on top of the existing one.
+     * Known errors are handled by the original.
+     *
+     * @param JoinPointInterface $joinPoint
+     * @return void
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Neos\Flow\Exception
+     * @throws \Neos\Flow\Mvc\Routing\Exception\MissingActionNameException
+     * @throws \Neos\Flow\Persistence\Exception\IllegalObjectTypeException
+     * @Flow\Around("method(Sandstorm\UserManagement\Controller\RegistrationController->activateAccountAction())")
+     */
+    public function enhancedRegistrationWorkflow(JoinPointInterface $joinPoint)
+    {
+        /* @var $registrationFlow RegistrationFlow */
+        $registrationFlow = $this->registrationFlowRepository->findOneByActivationToken($joinPoint->getMethodArgument('token'));
+        /* @var $registrationController RegistrationController */
+        $registrationController = $joinPoint->getProxy();
+
+        // let the original activateAccountAction handle errors
+        if (!$registrationFlow || !$registrationFlow->hasValidActivationToken()) {
+            return $joinPoint->getAdviceChain()->proceed($joinPoint);
         }
 
         $userMail = $registrationFlow->getEmail();
-        if (!$userMail) {
-            //TODO
-            return;
-        }
+        $userData = $registrationFlow->getAttributes();
+        $userName = $userData['firstName'] . ' ' . $userData['lastName'];
+        $isRegistrationForced = isset($userData['activateByForce']);
+        $membershipStatus = $this->getPersonMembershipStatus($userMail, $isRegistrationForced);
 
-        $this->connection = DriverManager::getConnection($this->databaseSettings);
-        $this->connection->connect();
-        $result = $this->connection->fetchArray("SELECT k_p_id FROM pkommunikation WHERE k_number = '" . $userMail . "'");
-        if (is_array($result) && !empty($result)) {
-            $member = $this->connection->fetchArray("SELECT p_nachname FROM personen WHERE p_id ='" . $result[0] . "'");
-            $subject = 'Berechtigt: ' . $member[0] . ', ' . $userMail;
-
-            $this->emailService->sendTemplateEmail(
-                'ActivationSuccessMember',
-                $subject,
-                [$userMail],
-                [
-                    'member' => $member
-                ],
-                'sandstorm_usermanagement_sender_email',
-                [], // cc
-                [], // bcc
-                [], // attachments
-                'sandstorm_usermanagement_replyTo_email'
-            );
-
+        if ($membershipStatus === self::IS_MEMBER) {
+            // create the user
             $this->userCreationService->createUserAndAccount($registrationFlow);
             $this->registrationFlowRepository->remove($registrationFlow);
             $this->persistenceManager->whitelistObject($registrationFlow);
+
+            $this->sendMail(
+                'ActivationSuccessFSH',
+                'FSH Website Registrierung von Mitglied erfolgt: ' . $userName,
+                [$this->fshRegistrationMailRecipient],
+                [
+                    'userName' => $userName,
+                    'userMail' => $userMail
+                ]
+            );
+
+            $this->sendMail(
+                'ActivationSuccessMember',
+                'Ihre Registrierung bei Frauenselbsthilfe',
+                [$userMail],
+                [
+                    'userName' => $userName,
+                    'userMail' => $userMail,
+                    'activationNotice' => $isRegistrationForced ? 'Ihre Registrierung wurde freigeschaltet!' : 'Ihre Registrierung war erfolgreich!'
+                ]
+            );
         } else {
-            //TODO no match
+            /**
+             * We want to enable the FSH to activate a new user account with 1 click.
+             * We reuse the current registrationFlow and create a new token for it,
+             * so the user cannot invoke this action again, but the FSH can.
+             * Also we set a 'force' attribute to force the Aspect to
+             * create the user this time.
+             * This should be safe, as the token is only available for FSH.
+             */
+
+            $registrationFlow->generateActivationToken();
+            $registrationFlow->setAttributes(array_merge(
+                $registrationFlow->getAttributes(),
+                ['activateByForce' => true,]
+            ));
+            $this->registrationFlowRepository->update($registrationFlow);
+            $this->persistenceManager->whitelistObject($registrationFlow);
+
+            // create new activation Link
+            $activationLink = $registrationController
+                ->getControllerContext()
+                ->getUriBuilder()
+                ->reset()
+                ->setCreateAbsoluteUri(true)
+                ->uriFor(
+                    'activateAccount',
+                    [
+                        'token' => $registrationFlow->getActivationToken()
+                    ],
+                    'Registration'
+                );
+
+            $this->sendMail(
+                'ActivationFailureFSH',
+                'FSH Website Registrierungsanfrage: ' . $userName,
+                [$this->fshRegistrationMailRecipient],
+                [
+                    'userName' => $userName,
+                    'userMail' => $userMail,
+                    'activationLink' => $activationLink,
+                    'memberStatus' => $this->membershipStatusToHumanReadableMsg($membershipStatus)
+                ]
+            );
+
+            $this->sendMail(
+                'ActivationFailureMember',
+                'Ihre Registrierung bei Frauenselbsthilfe',
+                [$userMail],
+                [
+                    'userName' => $userName,
+                    'userMail' => $userMail,
+                ]
+            );
         }
-        $this->connection->close();
     }
 
+    /**
+     * @param $userMail string
+     * @param $isForced bool
+     * @return string
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private function getPersonMembershipStatus($userMail, $isForced)
+    {
+        // no need to query the db if the registration is forced
+        if ($isForced) {
+            return self::IS_MEMBER;
+        }
+
+        // connect to member DB
+        $this->connection = DriverManager::getConnection($this->databaseSettings);
+        $this->connection->connect();
+        /**
+         * This query returns:
+         * 1 - if a person with that email exists and p_mitglied is 1
+         * 0 - if a person with that email exists and p_mitglied is 0
+         * false - if no person with that email exists
+         */
+        $result = $this->connection->fetchColumn(
+            "SELECT p_mitglied " .
+            "FROM pkommunikation " .
+            "INNER JOIN personen ON k_p_id = p_id " .
+            "WHERE k_number = '" . $userMail . "'"
+        );
+        $this->connection->close();
+
+        switch (true) {
+            case $result === '1':
+                return self::IS_MEMBER;
+            case $result === '0':
+                return self::NOT_MEMBER;
+            default:
+                return self::UNKNOWN;
+        }
+    }
+
+    private function sendMail($templateName, $subject, array $recipients, array $variables)
+    {
+        $this->emailService->sendTemplateEmail(
+            $templateName,
+            $subject,
+            $recipients,
+            $variables,
+            'sandstorm_usermanagement_sender_email',
+            [], // cc
+            [], // bcc
+            [], // attachments
+            'sandstorm_usermanagement_replyTo_email'
+        );
+    }
+
+    private function membershipStatusToHumanReadableMsg($status)
+    {
+        switch ($status) {
+            case self::UNKNOWN:
+                return 'Person ist unbekannt.';
+            case self::IS_MEMBER:
+                return 'Person ist Mitglied.';
+            case self::NOT_MEMBER:
+                return 'Person ist in Mitglieder DB, aber kein Mitglied.';
+            default:
+                throw new \Exception(
+                    'Unknown MembershipStatus: "' . $status .
+                    '"! Expected one of ' .
+                    [self::UNKNOWN, self::IS_MEMBER, self::NOT_MEMBER]
+                );
+        }
+    }
 }
